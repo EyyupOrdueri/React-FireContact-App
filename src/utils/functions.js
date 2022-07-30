fb1d13374a68edff5ba5628b4390fe49dd8c5550
eyyup <eyyupordueri@gmail.com>
2022-07-30 17:21:52 +0300

    File was created.

---
+++ src/utils/functions.js
@@ -0,0 +1,60 @@
+import firebase from "./firebase"
+import { getDatabase, set, push, ref, onValue, remove, update } from "firebase/database"
+import { useEffect, useState } from "react";
+import Toastify from "./toastify"
+
+// Adding info
+export const AddUser = (info) => {
+    const db = getDatabase(firebase);
+    const userRef = ref(db, "users/");
+    const newUserRef = push(userRef);
+    set(newUserRef, {
+        username: info.username,
+        phoneNumber: info.phoneNumber,
+        gender: info.gender
+    })
+}
+
+
+// Calling info
+
+export const useFetch = () => {
+
+    const [isLoading, setIsLoading] = useState()
+    const [contactList, setContactList] = useState()
+
+
+    useEffect(() => {
+        const db = getDatabase(firebase);
+
+        const userRef = ref(db, "users/");
+        onValue(userRef, (snapshot) => {
+            const data = snapshot.val();
+            const userArray = [];
+
+            for (let id in data) {
+                userArray.push({id, ...data[id]})
+            }
+            setContactList(userArray)
+            setIsLoading(false)
+        })
+
+    }, [])
+    return { isLoading, contactList }
+}
+
+export const DeleteUser = (id) => {
+    const db = getDatabase(firebase);
+    remove(ref(db, "users/" + id))
+    Toastify("Deleted successfully!")
+}
+
+export const UpdateUser = (info) => {
+    const db = getDatabase(firebase);
+    const updates = {}
+    updates["users/" + info.id] = info
+
+    Toastify("Updated successfully!")
+
+    return update(ref(db), updates)
+}
